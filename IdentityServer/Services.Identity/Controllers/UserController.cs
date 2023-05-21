af5b77c74fe9aa4702bb8984641fff4dd07bd95f
Merve Karahan <merve.karahan@paydustry.com>
2023-05-22 00:22:05 +0300

    UserController:

---
+++ IdentityServer/Services.Identity/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Services.Identity.Dtos;
+using Services.Identity.Models;
+using System.Threading.Tasks;
+
+namespace Services.Identity.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult>SingUp(SingUpDto singUpDto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = singUpDto.UserName,
+                Email = singUpDto.Email,
+                City = singUpDto.City,
+            };
+            var result = await _userManager.CreateAsync(user,singUpDto.Password);
+            if (!result.Succeeded)
+            {
+                return BadRequest();
+
+            }
+            return Ok() ;
+        }
+    }
+}
---
+++ IdentityServer/Services.Identity/Dtos/SingUpDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Services.Identity.Dtos
+{
+    public class SingUpDto
+    {
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string  City { get; set; }
+        public string Password { get; set; }
+    }
+}
